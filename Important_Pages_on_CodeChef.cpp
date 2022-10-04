c40020b814f9a05892cd75677abac6ea218177f1
Rushikesh Bhalerao <93110597+rushirao17@users.noreply.github.com>
2022-10-04 23:00:27 +0530

    Create Important_Pages_on_CodeChef.cpp

---
+++ Important_Pages_on_CodeChef.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,i;
+	int a,b;
+	cin>>a>>b;
+
+	    
+	if(a==0 and (b==0 or (b==1))){
+	    cout<<"https://www.codechef.com/practice"<<endl;}
+	else if(a==1 and b==0){
+	    cout<<"https://www.codechef.com/contests"<<endl;}
+	else{
+	    cout<<"https://discuss.codechef.com"<<endl;}
+	    
+	return 0;
+}
