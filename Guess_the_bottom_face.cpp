69836cb912e5a7055a9f2238e5153c61423b5b64
Rushikesh Bhalerao <93110597+rushirao17@users.noreply.github.com>
2022-10-06 00:52:37 +0530

    Create Guess_the_bottom_face.cpp

---
+++ Guess_the_bottom_face.cpp
@@ -0,0 +1,13 @@
+//Time exceed error solved
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n; cin>>n;
+	while(n--){
+	    int a; cin >> a;
+	    cout << 7-a << "\n";
+	}
+	return 0;
+}
