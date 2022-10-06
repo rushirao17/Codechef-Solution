4d2b0e01224c0240ceb92adfa3d2744c5d75e47e
Rushikesh Bhalerao <93110597+rushirao17@users.noreply.github.com>
2022-10-06 17:26:28 +0530

    Create Which_Division.cpp

---
+++ Which_Division.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include <cstdio>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,x,i,j;
+        cin>>a;
+        if(a<1600){cout<<3<<endl;}
+        else if(a<2000){cout<<2<<endl;}
+        else{cout<<1<<endl;}
+    }
+	return 0;
+}
