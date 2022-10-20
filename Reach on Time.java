48f6ee4e169f185ea2a510bde32c1a6880ab1fa6
Rushikesh Bhalerao <93110597+rushirao17@users.noreply.github.com>
2022-10-20 17:01:59 +0530

    Create Reach on Time.java

---
+++ Reach on Time.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef{
+	public static void main (String[] args) throws java.lang.Exception{
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++){
+		    int a=sc.nextInt();
+		    if(a>=30){System.out.println("YES");}
+		    else{System.out.println("NO");}
+		}
+	}
+}
